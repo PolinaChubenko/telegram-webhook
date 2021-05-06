dfbd34f2ac16468861a4b54bce5cc90b3e6dd757
Polina Chubenko <chubenko.pn@phystech.edu>
2021-05-06 20:54:33 +0300

    changing mode

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ DATABASE_URL = os.environ['DATABASE_URL']
 # cursor.execute(creation)
 
 
-def create_table():
+def create_db():
     connection = None
     try:
         connection = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -25,20 +25,18 @@ def create_table():
         creation = """CREATE TABLE chats_db (id SERIAL PRIMARY KEY, chat_id INTEGER, mode TEXT)"""
         cursor.execute(creation)
         cursor.close()
-    except psycopg2.DatabaseError:
+    except (Exception, psycopg2.DatabaseError):
         pass
-    # except (Exception, psycopg2.DatabaseError) as error:
-    #     print(error)
     finally:
         if connection is not None:
             connection.close()
 
 
 app = Flask(__name__)
-create_table()
+create_db()
 
 
-def add_value(chat_id):
+def db_add_value(chat_id):
     connection = None
     try:
         connection = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -51,10 +49,27 @@ def add_value(chat_id):
         cursor.close()
         connection.commit()
     except (Exception, psycopg2.DatabaseError) as error:
+        connection.rollback()
         print(error)
     finally:
         if connection is not None:
-            connection.rollback()
+
+            connection.close()
+
+
+def bd_change_value(chat_id, mode):
+    connection = None
+    try:
+        connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cursor = connection.cursor()
+        cursor.execute("""UPDATE chats_db SET mode = %s WHERE chat_id = %s""", (mode, chat_id))
+        cursor.close()
+        connection.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        connection.rollback()
+        print(error)
+    finally:
+        if connection is not None:
             connection.close()
 
 
@@ -72,11 +87,38 @@ def send_message(chat_id, text, parse_mode=None, reply_markup=None):
     requests.post(url, data=data)
 
 
+def is_command(r):
+    try:
+        if r.json["message"]["entities"][0]["type"] == "bot_command":
+            return True
+    except KeyError:
+        return False
+    return False
+
+
+def parse_command(chat_id, command):
+    text = command.split()
+    command = text[0]
+    text = ' '.join(text[1:])
+    if command == "/start":
+        send_message(chat_id, "I know command start")
+    elif command == "/help":
+        send_message(chat_id, "I know command help")
+    elif command == "/today" or command == "/tomorrow":
+        bd_change_value(chat_id, command[1:])
+        if text != "":
+            send_message(chat_id, "I changed mode for" + text)
+
+
 @app.route("/", methods=["POST"])
 def processing():
     chat_id = request.json["message"]["chat"]["id"]
-    add_value(chat_id)
-    send_message(chat_id, "Hello, sweetheart!")
+    db_add_value(chat_id)
+    if is_command(request):
+        text = request.json["message"]["text"]
+        parse_command(chat_id, text)
+    else:
+        send_message(chat_id, "Hello, sweetheart!")
     return {"ok": True}
 
 
