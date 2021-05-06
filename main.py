d3d38d9a9000b5aa6461410c00658447559086e6
Polina Chubenko <chubenko.pn@phystech.edu>
2021-05-06 15:15:55 +0300

    execute2

--- main.py
+++ main.py
@@ -22,19 +22,8 @@ cur = conn.cursor()
 print('PostgreSQL database version:')
 print(cur.execute('SELECT version()'))
 
-cur.execute("""CREATE TABLE albums
-                  (title text, artist text, release_date text,
-                   publisher text, media_type text)
-               """)
-cur.execute("""INSERT INTO albums
-                  VALUES ('Glow', 'Andy Hunter', '7/24/2012',
-                  'Xplore Records', 'MP3')""")
-conn.commit()
-albums = [('Exodus', 'Andy Hunter', '7/9/2002', 'Sparrow Records', 'CD'),
-          ('Until We Have Faces', 'Red', '2/1/2011', 'Essential Records', 'CD'),
-          ('The End is Where We Begin', 'Thousand Foot Krutch', '4/17/2012', 'TFKmusic', 'CD'),
-          ('The Good Life', 'Trip Lee', '4/10/2012', 'Reach Records', 'CD')]
-cur.executemany("INSERT INTO albums VALUES (?,?,?,?,?)", albums)
+cur.execute("""CREATE TABLE albums (title text, artist text, release_date text, publisher text, media_type text)""")
+cur.execute("""INSERT INTO albums VALUES ('Glow', 'Andy Hunter', '7/24/2012', 'Xplore Records', 'MP3')""")
 conn.commit()
 
 app = Flask(__name__)
